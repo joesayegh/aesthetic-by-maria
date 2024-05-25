87fc6e8b70b08a1215c4e539d4e9c5ffac43f3a2
Joe Sayegh <joesayegh@gmail.com>
2024-05-25 22:30:14 +0100

    implements dark mode

--- app.js
+++ app.js
@@ -40,6 +40,31 @@ for (var i = 0; i < primaryNavLinks.length; i++) {
 	});
 }
 
+///////////////////////////////////////////
+// MAKE SVG IMAGES INLINE TO TOGGLE CLASS
+///////////////////////////////////////////
+const convertImages = (query, callback) => {
+	const images = document.querySelectorAll(query);
+
+	images.forEach((image) => {
+		fetch(image.src)
+			.then((res) => res.text())
+			.then((data) => {
+				const parser = new DOMParser();
+				const svg = parser.parseFromString(data, "image/svg+xml").querySelector("svg");
+
+				if (image.id) svg.id = image.id;
+				if (image.className) svg.classList = image.classList;
+
+				image.parentNode.replaceChild(svg, image);
+			})
+			.then(callback)
+			.catch((error) => console.error(error));
+	});
+};
+
+convertImages("img");
+
 ///////////////////////////////////////
 // SWIPER
 ///////////////////////////////////////
